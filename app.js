58de8d0fe46b40999acc7689263e18dfaaba0c4c
LK MacBook Pro <nlk@MacBookPro.local>
2023-06-21 22:05:00 +0800

    added in Status feature

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ let deck = [];
 let dealerHand = [];
 let player1Hand = [];
 let player2Hand = [];
-let gameStatus = ''; // Added game status variable
+let player1Status = '';
+let player2Status = '';
+let dealerStatus = '';
 
 // Button elements
 const dealBtn = document.querySelector('#dealBtn');
@@ -27,7 +29,7 @@ const p1HitBtn = document.querySelector('#p1HitBtn');
 const p1StandBtn = document.querySelector('#p1StandBtn');
 const p2HitBtn = document.querySelector('#p2HitBtn');
 const p2StandBtn = document.querySelector('#p2StandBtn');
-const dealerStandBtn = document.querySelector('#dealerStandBtn'); // Added dealer Stand button
+const dealerStandBtn = document.querySelector('#dealerStandBtn');
 
 // UI elements
 const dealerCardsElement = document.querySelector('#dealerCards');
@@ -36,7 +38,9 @@ const player2CardsElement = document.querySelector('#player2Cards');
 const dealerTotalElement = document.querySelector('#dealerTotal');
 const player1TotalElement = document.querySelector('#player1Total');
 const player2TotalElement = document.querySelector('#player2Total');
-const gameStatusElement = document.querySelector('#gameStatus'); // Added game status element
+const player1StatusElement = document.querySelector('#player1Status');
+const player2StatusElement = document.querySelector('#player2Status');
+const dealerStatusElement = document.querySelector('#dealerStatus');
 
 // Disable/Enable buttons
 function disableButtons() {
@@ -46,7 +50,7 @@ function disableButtons() {
   p1StandBtn.disabled = true;
   p2HitBtn.disabled = true;
   p2StandBtn.disabled = true;
-  dealerStandBtn.disabled = true; // Disable dealer Stand button
+  dealerStandBtn.disabled = true;
 }
 
 function enableDealButton() {
@@ -59,7 +63,7 @@ function enablePlayerButtons() {
   p1StandBtn.disabled = false;
   p2HitBtn.disabled = false;
   p2StandBtn.disabled = false;
-  dealerStandBtn.disabled = false; // Enable dealer Stand button
+  dealerStandBtn.disabled = false;
 }
 
 // Deal a card
@@ -119,49 +123,15 @@ function displayTotal() {
   player2TotalElement.textContent = calculateTotal(player2Hand);
 }
 
-// Check for a blackjack
-function checkBlackjack(hand) {
-  return hand.length === 2 && calculateHandValue(hand) === 21;
-}
-
-// Check if hand busts
-function isBust(hand) {
-  return calculateHandValue(hand) > 21;
-}
-
-// Check game status
-function checkStatus() {
-  const dealerTotal = calculateTotal(dealerHand);
-  const player1Total = calculateTotal(player1Hand);
-  const player2Total = calculateTotal(player2Hand);
-
-  if (isBust(player1Hand)) {
-    gameStatus = 'Player 1 Busts! Player 2 and Dealer Win!';
-  } else if (isBust(player2Hand)) {
-    gameStatus = 'Player 2 Busts! Player 1 and Dealer Win!';
-  } else if (isBust(dealerHand)) {
-    gameStatus = 'Dealer Busts! Player 1 and Player 2 Win!';
-  } else if (player1Total > dealerTotal && player1Total <= 21) {
-    gameStatus = 'Player 1 Wins!';
-  } else if (player2Total > dealerTotal && player2Total <= 21) {
-    gameStatus = 'Player 2 Wins!';
-  } else if (dealerTotal > player1Total && dealerTotal > player2Total && dealerTotal <= 21) {
-    gameStatus = 'Dealer Wins!';
-  } else if (player1Total === dealerTotal && player2Total === dealerTotal) {
-    gameStatus = 'Draw!';
-  } else {
-    gameStatus = 'No winner yet!';
-  }
-
-  gameStatusElement.textContent = gameStatus;
-}
-
 // Deal initial cards
 function dealInitialCards() {
   // Clear existing hands
   dealerHand = [];
   player1Hand = [];
   player2Hand = [];
+  player1Status = '';
+  player2Status = '';
+  dealerStatus = '';
 
   // Deal 2 cards to each player and dealer
   for (let i = 0; i < 2; i++) {
@@ -177,13 +147,69 @@ function dealInitialCards() {
   displayTotal();
 }
 
+// Check for a blackjack
+function checkBlackjack(hand) {
+  return hand.length === 2 && calculateHandValue(hand) === 21;
+}
+
+// Check if hand busts
+function isBust(hand) {
+  return calculateHandValue(hand) > 21;
+}
+
+// Check if all players and dealer have clicked the Stand button
+function isAllStandClicked() {
+  return (
+    p1StandBtn.disabled &&
+    p2StandBtn.disabled &&
+    dealerStandBtn.disabled
+  );
+}
+
+// Determine the win, lose, or draw status
+function determineStatus() {
+  const dealerTotal = calculateHandValue(dealerHand);
+  const player1Total = calculateHandValue(player1Hand);
+  const player2Total = calculateHandValue(player2Hand);
+
+  if (isBust(dealerHand)) {
+    player1Status = 'Win';
+    player2Status = 'Win';
+    dealerStatus = 'Bust';
+  } else if (isBust(player1Hand) && isBust(player2Hand)) {
+    dealerStatus = 'Win';
+  } else if (player1Total === dealerTotal && player2Total === dealerTotal) {
+    player1Status = 'Draw';
+    player2Status = 'Draw';
+    dealerStatus = 'Draw';
+  } else if (player1Total > dealerTotal && player1Total <= 21) {
+    player1Status = 'Win';
+    dealerStatus = 'Lose';
+  } else if (player2Total > dealerTotal && player2Total <= 21) {
+    player2Status = 'Win';
+    dealerStatus = 'Lose';
+  } else if (dealerTotal > 21) {
+    player1Status = 'Win';
+    player2Status = 'Win';
+    dealerStatus = 'Bust';
+  } else {
+    player1Status = 'Lose';
+    player2Status = 'Lose';
+    dealerStatus = 'Win';
+  }
+
+  player1StatusElement.textContent = player1Status;
+  player2StatusElement.textContent = player2Status;
+  dealerStatusElement.textContent = dealerStatus;
+}
+
 // Handle Hit button click for Player 1
 function player1Hit() {
   const card = dealCard(player1Hand);
 
   if (isBust(player1Hand)) {
     disableButtons();
-    checkStatus();
+    // Handle player1 bust
   }
 
   updateUI();
@@ -196,7 +222,7 @@ function player2Hit() {
 
   if (isBust(player2Hand)) {
     disableButtons();
-    checkStatus();
+    // Handle player2 bust
   }
 
   updateUI();
@@ -209,7 +235,7 @@ function dealerHit() {
 
   if (isBust(dealerHand)) {
     disableButtons();
-    checkStatus();
+    // Handle dealer bust
   }
 
   updateUI();
@@ -220,23 +246,31 @@ function dealerHit() {
 function player1Stand() {
   p1HitBtn.disabled = true;
   p1StandBtn.disabled = true;
-  checkStatus();
+
+  if (isAllStandClicked()) {
+    determineStatus();
+  }
 }
 
 // Handle Stand button click for Player 2
 function player2Stand() {
   p2HitBtn.disabled = true;
   p2StandBtn.disabled = true;
-  checkStatus();
+
+  if (isAllStandClicked()) {
+    determineStatus();
+  }
 }
 
 // Handle Stand button click for Dealer
 function dealerStand() {
   dealerStandBtn.disabled = true;
-  hitBtn.disabled = true; // Disable the dealer's Hit button
-  checkStatus();
-}
+  hitBtn.disabled = true;
 
+  if (isAllStandClicked()) {
+    determineStatus();
+  }
+}
 
 // Handle Next Game button click
 function nextGame() {
@@ -244,13 +278,18 @@ function nextGame() {
   dealerHand = [];
   player1Hand = [];
   player2Hand = [];
-  gameStatus = ''; // Reset game status
+  player1Status = '';
+  player2Status = '';
+  dealerStatus = '';
 
   enableDealButton();
 
   updateUI();
   displayTotal();
-  gameStatusElement.textContent = ''; // Clear game status
+
+  player1StatusElement.textContent = '';
+  player2StatusElement.textContent = '';
+  dealerStatusElement.textContent = '';
 }
 
 // Event listeners
@@ -258,8 +297,8 @@ dealBtn.addEventListener('click', function () {
   dealInitialCards();
   dealBtn.disabled = true;
 });
-hitBtn.removeEventListener('click', player1Hit); // Remove player 1's hit functionality from the dealer's hit button
-hitBtn.addEventListener('click', dealerHit); // Add dealer's hit functionality
+hitBtn.removeEventListener('click', player1Hit);
+hitBtn.addEventListener('click', dealerHit);
 p1HitBtn.addEventListener('click', player1Hit);
 p2HitBtn.addEventListener('click', player2Hit);
 dealerStandBtn.addEventListener('click', dealerStand);
